509838207ef479a24d39453bfec9978cc6bb6eec
jimmy cruz <cruzperezjimmya@gmail.com>
2023-01-04 09:31:29 -0500

    se genero los componentes y modulos correspondientes y se establecio la ruta para el componente home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // se establece la ruta para redireccionamiento al componente home
+  {path:'', component:HomeComponent},
+  {path: '**', pathMatch: 'full', redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
